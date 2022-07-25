6638c215c37917854bf6c1c9c7f48b2087a5e522
tanishchugh01 <tanishchugh01@gmail.com>
2022-07-25 20:31:13 +0530

    first page complete

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import styled from "styled-components";
+import "./App.css";
+
+function App() {
+  const [a1, seta1] = useState("0");
+
+  const PTag = styled.p`
+    font-size: 10rem;
+  `;
+  const Button = styled.button`
+    background-color: blue;
+    height: 5rem;
+    width: 10rem;
+    font-size: 2rem;
+    border-radius: 1rem;
+    color: white;
+    :hover {
+      background-color: black;
+      // box-shadow:1px 0px 5px 6px black;
+    }
+    :active {
+      color: yellow;
+      background-color: grey;
+    }
+  `;
+  const Container = styled.div`
+    display: flex;
+    // flex-direction: col;
+    background-color: #a7fcce;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 100vh;
+    algign-items:stretch
+  `;
+  const Message = styled.p`
+    position: fixed;
+    background-color: white;
+    text-align: center;
+    padding: 3rem;
+    height: 50vh;
+    width: 50vw;
+    z-index: 2;
+    border-radius: 2rem;
+    display: ${a1 > 10 ? "block" : "none"};
+  `;
+
+  function generateRandom(setVar) {
+    setVar(Math.floor(Math.random() * 15));
+  }
+
+  return (
+    <Container>
+      <PTag>{a1}</PTag>
+      <Button onClick={() => generateRandom(seta1)}>Generate</Button>
+      <Message>
+        <h3>Target Achieved</h3>
+        <p>You will be redirected soon</p>
+      </Message>
+    </Container>
+  );
+}
+
+export default App;
